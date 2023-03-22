059de7d6e8c4c425529a077c8631bf8f4e9c5eea
NontoMhlongo <mntobeh@gmail.com>
2023-03-22 11:57:03 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main- Where the code start
+ * Return: 0 Success
+ */
+
+int main(void)
+{
+	printf("Size of a char: %c", sizeof(char));
+	printf("Size of an int: %d", sizeof(int));
+	printf("Size of long int: %ld", sizeof(long int));
+	printf("Size of a float: %f", sizeof(float));
+
+        return (0);
+}       
