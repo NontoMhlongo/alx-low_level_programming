bf2e7bf3398731590b4541e2c0aad81dbac8b49c
NontoMhlongo <mntobeh@gmail.com>
2023-03-23 16:17:49 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet in lowercase
+ *
+ * Return: 0 Success
+ */
+int main(void)
+{
+	char ch = 'a'
+		putchar("Alphabets in lowercase:\n");
+	while (c <= 'z')
+	{
+		putchar("%c", ch);
+		ch++;
+	}
+	return (0);
+}
