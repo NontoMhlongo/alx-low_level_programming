349da92c04db2181807fe2836fff37f3beb0a8b1
NontoMhlongo <mntobeh@gmail.com>
2023-03-27 09:14:28 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Start code
+ *
+ * Return: 0 Success
+ */
+int main(void)
+{
+	int f;
+
+	for (f = 0; f <= 9; f++)
+	{
+		putchar(f + 48);
+	}
+	putchar('\n');
+	return (0);
+}
