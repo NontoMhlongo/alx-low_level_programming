d455f2e24776ad90c80c58d7bd6feb8333228247
NontoMhlongo <mntobeh@gmail.com>
2023-03-27 07:27:39 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Start the code
+ *
+ * Return: 0 Success
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (i = 0; i > 0; i++)
+	{
+		printf("%d\n", i);
+	}
+	return (0);
+}
