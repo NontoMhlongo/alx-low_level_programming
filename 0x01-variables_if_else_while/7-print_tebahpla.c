7bc1e338d83cb3e268d93564fea8b4f77d9ac30f
NontoMhlongo <mntobeh@gmail.com>
2023-03-27 09:25:44 -0700

    reverse alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Strat the code
+ *
+ * Return: 0 Success
+ */
+int main(void)
+{
+	int v = 122;
+
+	while (v >= 97)
+	{
+		putchar(v);
+		v--;
+	}
+	putchar('\n');
+	return (0);
+}
