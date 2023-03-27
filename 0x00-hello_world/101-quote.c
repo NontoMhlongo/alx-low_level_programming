693daa4ef15987eace856a17243f27a3c4ad3d58
NontoMhlongo <mntobeh@gmail.com>
2023-03-27 04:15:41 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - Start of the code
- * Return 1 Error
+ * Return: 1 Success
  */
 
 int main(void)
 {
-	gets(and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
-	
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
