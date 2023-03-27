739f83fd850b54e24fa64ae975cbc1be79458933
NontoMhlongo <mntobeh@gmail.com>
2023-03-27 09:39:45 -0700

    print base

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Start the code
+ *
+ * Return: 0 Success
+ */
+int main(void)
+{
+	int d;
+	int c;
+	
+	for (d = 48; d <= 57; d++)
+	{
+		putchar(d);
+	}
+	for (c = 97; c <= 102; c++)
+	{
+		putchar(c);
+	}
+putchar('\n');	
+	return (0);
+}
