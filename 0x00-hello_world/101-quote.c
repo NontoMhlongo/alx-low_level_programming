dbcd3edf8861fe93bbdbbed2d9e36f9bd3a247d0
NontoMhlongo <mntobeh@gmail.com>
2023-03-27 04:31:36 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
 
 int main(void)
 {
-	fget("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	gets(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
